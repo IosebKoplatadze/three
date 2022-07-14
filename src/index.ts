878abb2bc598c97710fd2ddd0bbd8948f3969622
Ioseb Koplatadze <soso.kop@gmail.com>
2022-07-14 18:44:48 +0400

    responsive

--- src/index.ts
+++ src/index.ts
@@ -41,3 +41,9 @@ function animate() {
   cube.rotation.y += 0.005;
   renderer.render(scene, camera);
 }
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
