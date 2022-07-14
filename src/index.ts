2d464627c5cb20394a3ceb6478daa19b428820ab
Ioseb Koplatadze <soso.kop@gmail.com>
2022-07-14 18:32:06 +0400

    first earth and moon

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,43 @@
 import * as THREE from 'three';
 import './index.scss';
+import earthMap from './assets/earth.jpeg';
+import moonMap from './assets/moon.jpeg';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x999999);
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const earthTexture = new THREE.TextureLoader().load(earthMap);
+const geometry = new THREE.SphereGeometry(3);
+const material = new THREE.MeshStandardMaterial({ map: earthTexture });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+const moonTexture = new THREE.TextureLoader().load(moonMap);
+const geometry2 = new THREE.SphereGeometry(1);
+const material2 = new THREE.MeshStandardMaterial({ map: moonTexture });
+const moon = new THREE.Mesh(geometry2, material2);
+moon.position.setX(8);
+cube.add(moon);
+
+const light = new THREE.DirectionalLight(0xffffff, 1);
+light.position.set(4, 4, 4);
+scene.add(light);
+
 const camera = new THREE.PerspectiveCamera(
-  75,
+  60,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
+camera.position.z = 20;
+renderer.setAnimationLoop(animate);
 
 function animate() {
-  requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+  cube.rotation.y += 0.005;
   renderer.render(scene, camera);
 }
-
-animate();
